7c89d5fbf654d38eb1367c3ffd1cf9e95cde138d
bea <beata.piechowicz@student.uj.edu.pl>
2017-10-10 17:23:04 +0200

    Minor corrections

--- pollsApp/tests.py
+++ pollsApp/tests.py
@@ -17,7 +17,7 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_old_questio(self):
+    def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date is older than 1 day
         """
